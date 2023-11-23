7b4fe8e834453b3ef39b53232d3c35f9173960b5
BrendaDenhoff <brenda.denhoff@estudiantes.unahur.edu.ar>
2023-11-22 22:59:27 -0300

    Se cambia icono a flecha para poder ver a donde apunta, tambien se gira sobre su celda

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,71 @@ import React, { useState } from 'react';
 
 function App() {
   const gridSize = 5;
-  const posicionRobotInicial = { row: 0, col: 0 };
+  const posicionRobotInicial = { row: 0, col: 0, orientacion: 'derecha' };
 
   const [posicionRobot, setPosicionRobot] = useState(posicionRobotInicial);
 
   const moverRobot = (direction) => {
     const nuevaPosicion = { ...posicionRobot };
 
-    // eslint-disable-next-line default-case
     switch (direction) {
       case 'avanzar':
-        nuevaPosicion.col = (nuevaPosicion.col + 1) % gridSize;
+        moverEnDireccion(nuevaPosicion, true);
         break;
       case 'retroceder':
-        nuevaPosicion.col = (nuevaPosicion.col - 1) % gridSize;
+        moverEnDireccion(nuevaPosicion, false);
         break;
       case 'derecha':
-        nuevaPosicion.col = (nuevaPosicion.col + 1) % gridSize;
+        console.log(nuevaPosicion)
+        cambiarOrientacion(nuevaPosicion, 'derecha');
         break;
       case 'izquierda':
-        nuevaPosicion.col = (nuevaPosicion.col - 1) % gridSize;
+        cambiarOrientacion(nuevaPosicion, 'izquierda');
         break;
+      default:
+        // Comando no reconocido
+        return;
     }
 
     setPosicionRobot(nuevaPosicion);
+  };
+
+  const moverEnDireccion = (posicion, avanza) => {
+    const direccion = posicion.orientacion;
+    
+    const cambios = {
+      'abajo': { row: avanza ? 1 : -1, col: 0 },
+      'derecha': { row: 0, col: avanza ? 1 : -1 },
+      'izquierda': { row: 0, col: avanza ? -1 : 1 },
+      'arriba': { row: avanza ? -1 : 1, col: 0 },
+    };
+  
+    posicion.row = (posicion.row + cambios[direccion].row + gridSize) % gridSize;
+    posicion.col = (posicion.col + cambios[direccion].col + gridSize) % gridSize;
+  };
+
+  const cambiarOrientacion = (posicion, nuevaOrientacion) => {
+    const transicionesOrientacion = {
+      'derecha': { 'derecha': 'abajo', 'izquierda': 'arriba' },
+      'abajo': { 'derecha': 'izquierda', 'izquierda': 'derecha' },
+      'izquierda': { 'derecha': 'arriba', 'izquierda': 'abajo' },
+      'arriba': { 'derecha': 'derecha', 'izquierda': 'izquierda' },
+    };
+  
+    posicion.orientacion = transicionesOrientacion[posicion.orientacion][nuevaOrientacion];
+  };
+
+  const obtenerFlecha = () => {
+    switch (posicionRobot.orientacion) {
+      case 'derecha':
+        return <span>&rarr;</span>;
+      case 'izquierda':
+        return <span>&larr;</span>;
+      case 'arriba':
+        return <span>&uarr;</span>;
+      case 'abajo':
+        return <span>&darr;</span>;
+    }
   };
 
   const handleConsoleCommand = () => {
@@ -59,11 +100,8 @@ function App() {
             <tr key={rowIndex}>
               {[...Array(gridSize)].map((_, colIndex) => (
                 <td key={colIndex} style={{ border: '1px solid black', width: '30px', height: '30px' }}>
-                  {posicionRobot.row === rowIndex && posicionRobot.col === colIndex && (
-                    <span role="img" aria-label="robot">
-                      🤖
-                    </span>
-                  )}
+                  {posicionRobot.row === rowIndex && posicionRobot.col === colIndex &&
+                    obtenerFlecha()}
                 </td>
               ))}
             </tr>
