486019acbaf4dd7b8dba4e2ebbf29a4515d66b87
Laura Pereyra <lautiaguii@gmail.com>
2023-11-28 22:50:29 -0300

    socket

--- src/api.js
+++ src/api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3001');
 
 const Api = {
-  obtenerRegistros: () => {
+  obtenerRegistros: (actualizarDatosCallback) => {
+    socket.on('actualizarRegistros', (nuevosRegistros) => {
+      actualizarDatosCallback(nuevosRegistros);
+    });
+
     return axios
       .get('http://localhost:3001/comandos/')
       .then(function (response) {
