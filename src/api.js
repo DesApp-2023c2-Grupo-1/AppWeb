2922cea3c87b7e099c2cfa3b8d26f09b6f76a0db
MagaliAcosta <magali_solacosta@hotmail.com>
2024-07-03 22:52:00 -0300

    cambios despliegue 3

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('https://api-tn9p.onrender.com/3001');
+const socket = io('http://localhost:10000');
 
 const Api = {
   obtenerRegistros: () => {
