3d4359b04e40de428f7bc44291571161e068a555
BrendaDenhoff <brenda.denhoff@estudiantes.unahur.edu.ar>
2024-07-04 13:54:09 -0300

    vuelvo a localhost

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('https://api-ihgc.onrender.com/comandos');
+const socket = io('http://localhost:3001');
 
 const Api = {
   obtenerRegistros: () => {
