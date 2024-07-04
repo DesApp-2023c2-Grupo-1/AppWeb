a1bd4f642eeb1816144b160601bd3741db7a83fc
MagaliAcosta <magali_solacosta@hotmail.com>
2024-07-04 18:01:42 -0300

    3001

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('https://appweb-kzta.onrender.com');
+const socket = io('http://localhost:3001');
 
 const Api = {
   obtenerRegistros: () => {
