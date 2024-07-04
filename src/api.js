caff09637a8716b8d3a56265782ceb48cc3370c2
BrendaDenhoff <brenda.denhoff@estudiantes.unahur.edu.ar>
2024-07-03 23:02:10 -0300

    prueba

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('https://api-tn9p.onrender.com/3001');
+const socket = io('https://api-ihgc.onrender.com');
 
 const Api = {
   obtenerRegistros: () => {
